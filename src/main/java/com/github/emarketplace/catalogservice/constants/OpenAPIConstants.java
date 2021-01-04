9426e41a32fe30e0c806a594c6396f16ae38bef1
muhammed.shaheer <muhammed.shaheer@litmus7.com>
2021-01-04 14:56:38 +0530

    basic setup

---
+++ src/main/java/com/github/emarketplace/catalogservice/constants/OpenAPIConstants.java
@@ -0,0 +1,12 @@
+package com.github.emarketplace.catalogservice.constants;
+
+/**
+ * @author Muhammed Shaheer
+ * @since 04 January 2021
+ */
+
+public class OpenAPIConstants {
+    public static final String API_SPEC_VERSION = "1.0.0";
+    public static final String API_SPEC_TITLE = "Catalog Service API Specification";
+    public static final String API_SPEC_DESCRIPTION = "Catalog Service API Specification";
+}
